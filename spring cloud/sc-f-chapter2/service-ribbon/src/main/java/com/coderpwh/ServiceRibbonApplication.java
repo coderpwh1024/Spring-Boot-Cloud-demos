5258bc8aca7db651ae81ce766ced75d0dd03aecb
coder-PengWenHao <pengwenhao@aliyun.com>
2018-08-06 23:06:17 +0800

    init ch2

---
+++ spring cloud/sc-f-chapter2/service-ribbon/src/main/java/com/coderpwh/ServiceRibbonApplication.java
@@ -0,0 +1,12 @@
+package com.coderpwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRibbonApplication.class, args);
+	}
+}
