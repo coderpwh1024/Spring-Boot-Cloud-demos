fcd39a111c9ff43b61c5b761ab527cda5b9e3991
coder-PengWenHao <pengwenhao@aliyun.com>
2018-08-23 22:25:18 +0800

    update spring cloud

--- spring cloud/sc-f-chapter8/config-client/src/main/java/com/coderpwh/configclient/ConfigClientApplication.java
+++ spring cloud/sc-f-chapter8/config-client/src/main/java/com/coderpwh/configclient/ConfigClientApplication.java
@@ -2,6 +2,7 @@ package com.coderpwh.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RefreshScope
 @EnableDiscoveryClient
+@EnableAutoConfiguration
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
