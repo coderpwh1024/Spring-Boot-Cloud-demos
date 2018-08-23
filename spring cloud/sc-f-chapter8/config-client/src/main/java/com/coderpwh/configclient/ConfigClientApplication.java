ead0eba419bb860a0ea85b35e3270aac04421ef3
coder-PengWenHao <pengwenhao@aliyun.com>
2018-08-23 22:04:55 +0800

    update spring cloud

---
+++ spring cloud/sc-f-chapter8/config-client/src/main/java/com/coderpwh/configclient/ConfigClientApplication.java
@@ -0,0 +1,31 @@
+package com.coderpwh.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@EnableEurekaClient
+@RestController
+@RefreshScope
+@EnableDiscoveryClient
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+
+	@Value("${foo}")
+	String foo;
+
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
+	}
+
+}
