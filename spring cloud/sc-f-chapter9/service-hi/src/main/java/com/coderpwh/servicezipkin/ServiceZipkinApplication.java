ac9e8af5508fc7e06b9311926991b1ae6d5cec83
coder-PengWenHao <pengwenhao@aliyun.com>
2018-08-29 22:37:23 +0800

    init chapter9

---
+++ spring cloud/sc-f-chapter9/service-hi/src/main/java/com/coderpwh/servicezipkin/ServiceZipkinApplication.java
@@ -0,0 +1,12 @@
+package com.coderpwh.servicezipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceZipkinApplication.class, args);
+	}
+}
