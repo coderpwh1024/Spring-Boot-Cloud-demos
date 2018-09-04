4e67dc908ebab10d7ee6d61547e9237558bd237f
coderpwh <pengwenhao@aliyun.com>
2018-09-04 11:38:35 +0800

    Add files via upload
    update mq code

--- Message Queue/RabbitMQ/demo2/src/main/java/com/coderpwh/demo2/Send.java
+++ Message Queue/RabbitMQ/demo2/src/main/java/com/coderpwh/demo2/Send.java
@@ -1,8 +1,50 @@
 package com.coderpwh.demo2;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class Send {
 
-    public static void main(String[] args) {
-        
+    private final static String TASK_QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+        String message = getMessage(args);
+        channel.basicPublish("", "hello", null, message.getBytes());
+        System.out.println("[x] Sent'" + message + "'");
+
+
     }
+
+    private static String getMessage(String[] strings) {
+        if (strings.length < 1) {
+            return "Hello World!";
+        }
+        return joinStrings(strings, "");
+    }
+
+    private static String joinStrings(String[] strings, String delimiter) {
+
+        int length = strings.length;
+        if (length == 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder(strings[0]);
+        for (int i = 1; i < length; i++) {
+            sb.append(delimiter).append(strings[1]);
+        }
+        return sb.toString();
+    }
+
+
 }
---
+++ Message Queue/RabbitMQ/demo2/src/main/java/com/coderpwh/demo2/Worker.java
@@ -0,0 +1,57 @@
+package com.coderpwh.demo2;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+
+public class Worker {
+
+    private final static String TASK_QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+        final Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println("[x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println("[x] Done");
+                }
+
+            }
+        };
+
+        boolean autoAck = true;
+        channel.basicConsume(TASK_QUEUE_NAME, autoAck, consumer);
+
+    }
+
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                Thread.sleep(1000);
+            }
+        }
+    }
+
+}
