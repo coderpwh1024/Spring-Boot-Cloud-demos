a168a1b87a9ed80cf3896d40afe2da8192a807eb
coderpwh <pengwenhao@aliyun.com>
2018-09-05 11:39:16 +0800

    Add files via upload
    init spirng cloud sc-f-chapter9

---
+++ spring cloud/sc-f-chapter10/eureka-server/src/main/java/com/coderpwh/eurekaserve/EurekaServeApplication.java
@@ -0,0 +1,12 @@
+package com.coderpwh.eurekaserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServeApplication.class, args);
+    }
+}
