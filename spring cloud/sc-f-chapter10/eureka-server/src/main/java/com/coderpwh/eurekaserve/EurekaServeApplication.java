10b0c820953f41ecc15a2a553849b3047eedcf61
coder-PengWenHao <pengwenhao@aliyun.com>
2018-09-05 22:20:03 +0800

    update spring cloud code

--- spring cloud/sc-f-chapter10/eureka-server/src/main/java/com/coderpwh/eurekaserve/EurekaServeApplication.java
+++ spring cloud/sc-f-chapter10/eureka-server/src/main/java/com/coderpwh/eurekaserve/EurekaServeApplication.java
@@ -2,8 +2,10 @@ package com.coderpwh.eurekaserve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServeApplication {
 
     public static void main(String[] args) {
--- spring cloud/sc-f-chapter10/service-hi/src/main/java/com/coderpwh/servicehi/ServiceHiApplication.java
+++ spring cloud/sc-f-chapter10/service-hi/src/main/java/com/coderpwh/servicehi/ServiceHiApplication.java
@@ -1,12 +1,32 @@
 package com.coderpwh.servicehi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
 public class ServiceHiApplication {
+
+    @Value("${server.port}")
+    String port;
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceHiApplication.class, args);
     }
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+
+        return "hi " + name + " , i am from port:" + port;
+    }
+
+
 }
