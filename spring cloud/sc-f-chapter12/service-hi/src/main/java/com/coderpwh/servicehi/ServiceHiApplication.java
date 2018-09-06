9e2a49f89690c98a3db74b614f688e2f35ed6739
coderpwh <pengwenhao@aliyun.com>
2018-09-06 14:29:16 +0800

    Add files via upload
    update spring cloud sc-f-chater12 code

--- spring cloud/sc-f-chapter12/service-hi/src/main/java/com/coderpwh/servicehi/ServiceHiApplication.java
+++ spring cloud/sc-f-chapter12/service-hi/src/main/java/com/coderpwh/servicehi/ServiceHiApplication.java
@@ -1,12 +1,41 @@
 package com.coderpwh.servicehi;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
+@EnableHystrix
+@EnableHystrixDashboard
 public class ServiceHiApplication {
+
+    @Value("${server.port}")
+    String port;
+
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceHiApplication.class, args);
     }
+
+    @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String home(@RequestParam(value = "nmae", defaultValue = "forezp") String name) {
+
+        return "hi " + name + " ,i am form port:" + port;
+    }
+
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
+    }
+
+
 }
