211523af231389ce80f585082b9c721c54003f46
coderpwh <pengwenhao@aliyun.com>
2018-09-07 10:56:37 +0800

    Add files via upload
    update spring cloud code

--- spring cloud/sc-f-chapter13/service-lucy/src/main/java/com/coderpwh/servicelucy/ServiceLucyApplication.java
+++ spring cloud/sc-f-chapter13/service-lucy/src/main/java/com/coderpwh/servicelucy/ServiceLucyApplication.java
@@ -4,10 +4,22 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ServiceLucyApplication {
 
     @Value("${server.port}")
