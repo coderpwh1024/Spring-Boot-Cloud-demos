97d09014076bff771ffcb7bcba643ebe3eee0795
coder-PengWenHao <pengwenhao@aliyun.com>
2018-09-13 22:59:19 +0800

    update pom

--- spring cloud/sleuth/gateway-service/src/main/java/com/coderpwh/gatewayservice/GatewayServiceApplication.java
+++ spring cloud/sleuth/gateway-service/src/main/java/com/coderpwh/gatewayservice/GatewayServiceApplication.java
@@ -2,11 +2,15 @@ package com.coderpwh.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class GatewayServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
 }
--- spring cloud/sleuth/zipkin-server/src/main/java/com/coderpwh/zipkinserver/ZipkinServerApplication.java
+++ spring cloud/sleuth/zipkin-server/src/main/java/com/coderpwh/zipkinserver/ZipkinServerApplication.java
@@ -2,11 +2,15 @@ package com.coderpwh.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
 public class ZipkinServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZipkinServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
 }
