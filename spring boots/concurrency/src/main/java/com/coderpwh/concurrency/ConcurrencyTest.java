3e42a831fea0fc2d0362a0430807af5f18141eac
coderpwh <pengwenhao@aliyun.com>
2018-10-18 10:28:22 +0800

    Update ConcurrencyTest.java
    repair  ConcurrencyTest.java   bug

--- spring boots/concurrency/src/main/java/com/coderpwh/concurrency/ConcurrencyTest.java
+++ spring boots/concurrency/src/main/java/com/coderpwh/concurrency/ConcurrencyTest.java
@@ -1,13 +1,17 @@
-package com.coderpwh.concurrency;
+ package com.coderpwh.concurrency;
 
 import com.coderpwh.concurrency.annoations.NotThreadSafe;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 
-import java.util.concurrent.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 @Slf4j
 @NotThreadSafe
-public class ConcurrencyTest {
+class ConcurrencyTest {
 
     // 请求总数
     public static int clientTotal = 5000;
@@ -17,33 +21,47 @@ public class ConcurrencyTest {
 
     public static int count = 0;
 
-//    public static   Logger log = org.slf4j.LoggerFactory.getLogger("aa");
+    public static Logger log = org.slf4j.LoggerFactory.getLogger("aa");
 
     public static void main(String[] args) throws Exception {
         ExecutorService executorService = Executors.newCachedThreadPool();
-//        ExecutorService executorService = new  ThreadPoolExecutor(200,300,1L,TimeUnit.DAYS,new LinkedBlockingDeque());
         final Semaphore semaphore = new Semaphore(threadTotal);
-        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        //        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
         for (int i = 0; i < clientTotal; i++) {
-            executorService.execute(() -> {
+
+            // jdk1.8 lambda
+             executorService.execute(() -> {
                 try {
                     semaphore.acquire();
                     add();
                     semaphore.release();
                 } catch (Exception e) {
-
-//                    log.error("exception:",e);
-
+                    log.error("exception:", e);
                 }
                 countDownLatch.countDown();
-
             });
-            countDownLatch.await();
-            executorService.shutdown();
-//             log.info("count:{}",count);
-            System.out.println("count:" + count);
-        }
 
+             //
+          /*  executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        semaphore.acquire();
+                        add();
+                        semaphore.release();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    countDownLatch.countDown();
+                }
+            });*/
+
+
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}", count);
     }
 
     public static void add() {
