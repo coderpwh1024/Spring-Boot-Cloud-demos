7dd763073f27513b452055313b61a827b669be4d
coder-PengWenHao <pengwenhao@aliyun.com>
2018-10-23 22:29:16 +0800

    update concurrency

---
+++ spring boots/concurrency/src/main/java/com/coderpwh/concurrency/example/count/CountExample4.java
@@ -0,0 +1,72 @@
+package com.coderpwh.concurrency.example.count;
+
+import com.coderpwh.concurrency.annoations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@NotThreadSafe
+public class CountExample4 {
+
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static volatile int count = 0;
+
+    public static Logger log = org.slf4j.LoggerFactory.getLogger("aa");
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        //        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+
+            // jdk1.8 lambda
+             executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception:", e);
+                }
+                countDownLatch.countDown();
+            });
+
+             //
+          /*  executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        semaphore.acquire();
+                        add();
+                        semaphore.release();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    countDownLatch.countDown();
+                }
+            });*/
+
+
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}", count);
+    }
+
+    public static void add() {
+        count++;
+    }
+
+
+}
