ef85642dbdbbadd79c3c23e741fedf7ee4834cb6
coder-PengWenHao <pengwenhao@aliyun.com>
2018-11-05 21:56:49 +0800

    add concurrent

---
+++ spring boots/concurrency/src/main/java/com/coderpwh/concurrency/example/concurrent/CopyOnWriteArrayListExample.java
@@ -0,0 +1,54 @@
+package com.coderpwh.concurrency.example.concurrent;
+
+import com.coderpwh.concurrency.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+
+import java.util.List;
+import java.util.concurrent.*;
+
+@Slf4j
+@ThreadSafe
+public class CopyOnWriteArrayListExample {
+
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+    public static Logger log = org.slf4j.LoggerFactory.getLogger("aa");
+
+//    private static List<Integer> list = Collections.synchronizedList(Lists.newArrayList());
+
+    private static List<Integer> list = new CopyOnWriteArrayList<>();
+
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            // jdk1.8 lambda
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception:", e);
+                }
+                countDownLatch.countDown();
+            });
+
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("size:{}", list.size());
+
+    }
+
+    public static void update(int i) {
+        list.add(i);
+    }
+}
