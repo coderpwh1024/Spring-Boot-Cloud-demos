492d166b241cb608e8e9648c31a5930da3ffdcdd
coder-PengWenHao <pengwenhao@aliyun.com>
2018-11-07 22:32:11 +0800

    update aqs

---
+++ spring boots/concurrency/src/main/java/com/coderpwh/concurrency/example/aqs/CyclicBarrierExample3.java
@@ -0,0 +1,42 @@
+
+package com.coderpwh.concurrency.example.aqs;
+
+import org.slf4j.Logger;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CyclicBarrierExample3 {
+    public static Logger log = org.slf4j.LoggerFactory.getLogger("aa");
+    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(5, () -> {
+        log.info("callback() is running");
+    });
+
+
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            final int threadNum = i;
+            Thread.sleep(1000);
+            executorService.execute(() -> {
+                try {
+                    race(threadNum);
+                } catch (Exception e) {
+                    log.info("exception", e);
+                }
+
+            });
+        }
+
+
+    }
+
+    public static void race(int threadNum) throws Exception {
+        Thread.sleep(1000);
+        log.info("{} is ready", threadNum);
+        cyclicBarrier.await();
+        log.info("{} continue");
+    }
+}
