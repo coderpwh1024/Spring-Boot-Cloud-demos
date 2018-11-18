e7a8be46a1130e2dc9764a1bc289c3a3dc322b83
coder-PengWenHao <pengwenhao@aliyun.com>
2018-11-18 22:51:28 +0800

    update threadpool

--- spring boots/concurrency/src/main/java/com/coderpwh/concurrency/example/threadPool/ThreadToolExample4.java
+++ spring boots/concurrency/src/main/java/com/coderpwh/concurrency/example/threadPool/ThreadToolExample4.java
@@ -3,8 +3,12 @@ package com.coderpwh.concurrency.example.threadPool;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 
-import java.util.concurrent.ExecutorService;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class ThreadToolExample4 {
@@ -13,19 +17,33 @@ public class ThreadToolExample4 {
 
     public static void main(String[] args) {
 
-        ExecutorService executorService = Executors.newScheduledThreadPool(5);
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(5);
 
-        for (int i = 0; i < 10; i++) {
-            final int index = i;
-            executorService.execute(new Runnable() {
-                @Override
-                public void run() {
-                    log.info("task:{}", index);
-                }
-            });
 
-        }
-        executorService.shutdown();
+       /* executorService.schedule(new Runnable() {
+            @Override
+            public void run() {
+                log.warn("schedule run");
+            }
+        }, 3, TimeUnit.SECONDS);*/
+
+
+        executorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                log.warn("schedule run");
+            }
+        }, 1, 3, TimeUnit.SECONDS);
+
+//        executorService.shutdown();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                log.warn("schedule run");
+            }
+        }, new Date(), 5 * 1000);
+
 
     }
 }
