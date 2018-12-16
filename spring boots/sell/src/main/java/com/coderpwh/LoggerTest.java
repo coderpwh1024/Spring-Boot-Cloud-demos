cae9da53a6907126b906a6b6b15abaa4b4080fcc
coder-PengWenHao <pengwenhao@aliyun.com>
2018-12-16 23:23:18 +0800

    add sell

--- spring boots/sell/src/main/java/com/coderpwh/LoggerTest.java
+++ spring boots/sell/src/main/java/com/coderpwh/LoggerTest.java
@@ -1,6 +1,34 @@
 package com.coderpwh;
 
- 
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+
 public class LoggerTest {
+
+//    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+    @Test
+    public void test1() {
+
+        String name = "coderpwh";
+        String password = "123456";
+
+        log.debug("debug...");
+//        log.info("info......");
+        log.info("name: " + name + ", password:" + password);
+        log.info("name: {},password:{}", name, password);
+        log.error("error....");
+
+
+    }
 
 }
