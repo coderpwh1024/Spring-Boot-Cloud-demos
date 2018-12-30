1cdc282953e9adee840e8bb51b4f458e920ed6bc
coder-PengWenHao <pengwenhao@aliyun.com>
2018-12-31 00:01:20 +0800

    add package

--- spring boots/sell/src/main/java/com/coderpwh/dataObject/ProductCategory.java
+++ spring boots/sell/src/main/java/com/coderpwh/dataObject/ProductCategory.java
@@ -1,10 +1,46 @@
 package com.coderpwh.dataObject;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * @author coderpwh
  * @create 2018-12-30 23:42
  * @desc ${DESCRIPTION}
  **/
+@Entity
 public class ProductCategory {
-    
+
+    @Id
+    @GeneratedValue
+    private Integer categoryId;
+
+    private String categoryName;
+
+    private Integer categoryType;
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Integer getCategoryType() {
+        return categoryType;
+    }
+
+    public void setCategoryType(Integer categoryType) {
+        this.categoryType = categoryType;
+    }
 }
