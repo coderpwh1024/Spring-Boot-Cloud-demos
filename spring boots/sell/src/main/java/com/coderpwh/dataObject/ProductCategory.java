29552a92965502b000e3a14d60453ea38c954a0f
coder-PengWenHao <pengwenhao@aliyun.com>
2019-01-08 23:29:46 +0800

    update sell

--- spring boots/sell/src/main/java/com/coderpwh/dataObject/ProductCategory.java
+++ spring boots/sell/src/main/java/com/coderpwh/dataObject/ProductCategory.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.DynamicUpdate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Date;
 
 /**
  * 类目
@@ -17,6 +18,7 @@ import javax.persistence.Id;
 @Data
 public class ProductCategory {
 
+
     /** 类目id. */
     @Id
     @GeneratedValue
@@ -27,6 +29,51 @@ public class ProductCategory {
 
     /** 类目编号. */
     private Integer categoryType;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Integer getCategoryType() {
+        return categoryType;
+    }
+
+    public void setCategoryType(Integer categoryType) {
+        this.categoryType = categoryType;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
 
     public ProductCategory() {
     }
--- spring boots/sell/src/test/java/com/coderpwh/repository/ProductCategoryRepositoryTest.java
+++ spring boots/sell/src/test/java/com/coderpwh/repository/ProductCategoryRepositoryTest.java
@@ -8,8 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.transaction.Transactional;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -23,24 +23,27 @@ public class ProductCategoryRepositoryTest {
     @Autowired
     private ProductCategoryRepository repository;
 
-    @Test
+
     public void findOneTest() {
         ProductCategory productCategory = repository.findOne(1);
         System.out.println(productCategory.toString());
     }
 
     @Test
-    @Transactional
     public void saveTest() {
-        ProductCategory productCategory = new ProductCategory("男生最爱", 4);
-        ProductCategory result = repository.save(productCategory);
-        Assert.assertNotNull(result);
-//        Assert.assertNotEquals(null, result);
+        ProductCategory productCategory = new ProductCategory();
+        Date date = new Date();
+        productCategory.setCategoryName("女生最爱");
+        productCategory.setCategoryType(3);
+        productCategory.setCategoryId(2);
+        productCategory.setCreateTime(date);
+        productCategory.setUpdateTime(date);
+        repository.save(productCategory);
     }
 
-    @Test
+
     public void findByCategoryTypeInTest() {
-        List<Integer> list = Arrays.asList(2,3,4);
+        List<Integer> list = Arrays.asList(2, 3, 4);
 
         List<ProductCategory> result = repository.findByCategoryTypeIn(list);
         Assert.assertNotEquals(0, result.size());
