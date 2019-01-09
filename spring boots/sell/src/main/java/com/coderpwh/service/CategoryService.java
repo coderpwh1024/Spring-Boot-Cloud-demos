f213ac07101d582eb7b15b215c7400b147166e53
coder-PengWenHao <pengwenhao@aliyun.com>
2019-01-09 23:44:02 +0800

    update sell service

--- spring boots/sell/src/main/java/com/coderpwh/service/CategoryService.java
+++ spring boots/sell/src/main/java/com/coderpwh/service/CategoryService.java
@@ -1,11 +1,25 @@
 package com.coderpwh.service;
 
+import com.coderpwh.dataobject.ProductCategory;
+
+import java.util.List;
+
 /**
- *   类名
+ * 类名
+ *
  * @author coderpwh
  * @create 2019-01-09 23:41
  * @desc ${DESCRIPTION}
  **/
 public interface CategoryService {
+
+    ProductCategory findOne(Integer categoryId);
+
+    List<ProductCategory> findAll();
+
+    List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
+
+    ProductCategory save(ProductCategory productCategory);
+
 
 }
---
+++ spring boots/sell/src/main/java/com/coderpwh/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,35 @@
+package com.coderpwh.service.impl;
+
+import com.coderpwh.dataobject.ProductCategory;
+import com.coderpwh.service.CategoryService;
+
+import java.util.List;
+
+/**
+ * @author coderpwh
+ * @create 2019-01-09 23:42
+ * @desc ${DESCRIPTION}
+ **/
+public class CategoryServiceImpl implements CategoryService {
+
+
+    @Override
+    public ProductCategory findOne(Integer categoryId) {
+        return null;
+    }
+
+    @Override
+    public List<ProductCategory> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList) {
+        return null;
+    }
+
+    @Override
+    public ProductCategory save(ProductCategory productCategory) {
+        return null;
+    }
+}
