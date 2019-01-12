18f4d642447b7b74ac270327cd248ee4edaa396b
coder-PengWenHao <pengwenhao@aliyun.com>
2019-01-12 23:48:44 +0800

    update sell

--- spring boots/sell/src/test/java/com/coderpwh/service/impl/CategoryServiceImplTest.java
+++ spring boots/sell/src/test/java/com/coderpwh/service/impl/CategoryServiceImplTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -38,8 +39,18 @@ public class CategoryServiceImplTest {
 
     @Test
     public void findByCategoryTypeIn() throws Exception {
-        List<ProductCategory> productCategoryList = categoryService.findByCategoryTypeIn(Arrays.asList(1,2,3,4));
+        List<ProductCategory> productCategoryList = categoryService.findByCategoryTypeIn(Arrays.asList(1, 2, 3, 4));
         Assert.assertNotEquals(0, productCategoryList.size());
+    }
+
+    @Test
+    public void save() throws Exception {
+        ProductCategory productCategory = new ProductCategory("男生专享", 10);
+        Date date = new Date();
+        productCategory.setUpdateTime(date);
+        productCategory.setCreateTime(date);
+        ProductCategory result = categoryService.save(productCategory);
+        Assert.assertNotNull(result);
     }
 
 
