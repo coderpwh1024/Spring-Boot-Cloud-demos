20eaed7e95c1f437fe363e11262784cbaacc1747
coder-PengWenHao <pengwenhao@aliyun.com>
2019-01-15 23:01:24 +0800

    update sell

---
+++ spring boots/sell/src/main/java/com/coderpwh/service/impl/ProductServiceImpl.java
@@ -0,0 +1,66 @@
+package com.coderpwh.service.impl;
+
+import com.coderpwh.Dto.CartDTO;
+import com.coderpwh.dataobject.ProductInfo;
+import com.coderpwh.enums.ProductStatusEnum;
+import com.coderpwh.repository.ProductInfoRepository;
+import com.coderpwh.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author coderpwh
+ * @create 2019-01-15 22:55
+ * @desc ${DESCRIPTION}
+ **/
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductInfoRepository repository;
+
+    @Override
+    public ProductInfo findOne(String productId) {
+        return repository.findOne(productId);
+    }
+
+    @Override
+    public List<ProductInfo> findUpAll() {
+        return repository.findByProductStatus(ProductStatusEnum.UP.getCode());
+    }
+
+    @Override
+    public Page<ProductInfo> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    @Override
+    public ProductInfo save(ProductInfo productInfo) {
+        return repository.save(productInfo);
+    }
+
+    @Override
+    public void increaseStock(List<CartDTO> cartDTOList) {
+
+    }
+
+    @Override
+    public void decreaseStock(List<CartDTO> cartDTOList) {
+
+    }
+
+    @Override
+    public ProductInfo onSale(String productId) {
+        return null;
+    }
+
+    @Override
+    public ProductInfo offSale(String productId) {
+        return null;
+    }
+
+}
