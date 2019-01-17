e28ec12c22b3630dc9b6a208e33e4696b90ffa4c
coder-PengWenHao <pengwenhao@aliyun.com>
2019-01-17 22:38:42 +0800

    update sell

--- spring boots/sell/src/test/java/com/coderpwh/service/impl/ProductServiceImplTest.java
+++ spring boots/sell/src/test/java/com/coderpwh/service/impl/ProductServiceImplTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 /**
  * @author coderpwh
  * @create 2019-01-17 22:32
@@ -25,4 +27,11 @@ public class ProductServiceImplTest {
         ProductInfo productInfo = productService.findOne("123456");
         Assert.assertEquals("123456", productInfo.getProductId());
     }
+
+    @Test
+    public void findUpAll() throws Exception {
+        List<ProductInfo> productInfoList = productService.findUpAll();
+        Assert.assertNotEquals(0, productInfoList.size());
+    }
+
 }
