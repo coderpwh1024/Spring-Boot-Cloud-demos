14e7b96172c5ded702687648a0598b7243e4d36b
coder-PengWenHao <pengwenhao@aliyun.com>
2019-01-17 22:35:22 +0800

    update sell

---
+++ spring boots/sell/src/test/java/com/coderpwh/service/impl/ProductServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.coderpwh.service.impl;
+
+import com.coderpwh.dataobject.ProductInfo;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author coderpwh
+ * @create 2019-01-17 22:32
+ * @desc ${DESCRIPTION}
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductServiceImplTest {
+
+    @Autowired
+    private ProductServiceImpl productService;
+
+    @Test
+    public void findOne() throws Exception {
+        ProductInfo productInfo = productService.findOne("123456");
+        Assert.assertEquals("123456", productInfo.getProductId());
+    }
+}
