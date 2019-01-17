d6e0318d78110d1a39a7fc6d511086a881f7053e
coder-PengWenHao <pengwenhao@aliyun.com>
2019-01-17 22:42:27 +0800

    update sell

--- spring boots/sell/src/test/java/com/coderpwh/service/impl/ProductServiceImplTest.java
+++ spring boots/sell/src/test/java/com/coderpwh/service/impl/ProductServiceImplTest.java
@@ -1,13 +1,18 @@
 package com.coderpwh.service.impl;
 
 import com.coderpwh.dataobject.ProductInfo;
+import com.coderpwh.enums.ProductStatusEnum;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -33,5 +38,33 @@ public class ProductServiceImplTest {
         List<ProductInfo> productInfoList = productService.findUpAll();
         Assert.assertNotEquals(0, productInfoList.size());
     }
+
+    @Test
+    public void findAll() throws Exception {
+        PageRequest request = new PageRequest(0, 2);
+        Page<ProductInfo> productInfoPage = productService.findAll(request);
+        Assert.assertNotEquals(0, productInfoPage.getTotalElements());
+    }
+
+
+    @Test
+    public void save() throws Exception {
+        ProductInfo productInfo = new ProductInfo();
+        Date date = new Date();
+        productInfo.setProductId("123457");
+        productInfo.setProductName("皮皮虾");
+        productInfo.setProductPrice(new BigDecimal(3.2));
+        productInfo.setProductStock(100);
+        productInfo.setProductDescription("很好吃的虾");
+        productInfo.setProductIcon("http://xxxxx.jpg");
+        productInfo.setProductStatus(ProductStatusEnum.DOWN.getCode());
+        productInfo.setCategoryType(2);
+        productInfo.setUpdateTime(date);
+        productInfo.setCreateTime(date);
+
+        ProductInfo result = productService.save(productInfo);
+        Assert.assertNotNull(result);
+    }
+
 
 }
