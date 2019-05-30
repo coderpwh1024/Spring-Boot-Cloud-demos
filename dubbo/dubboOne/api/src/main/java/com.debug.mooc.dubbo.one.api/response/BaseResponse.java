64ca6377f11f876e3e9b9b78f5525cec9126ff33
coderpwh <pengwenhao@aliyun.com>
2019-05-30 14:57:57 +0800

    Add files via upload
    add response package

---
+++ dubbo/dubboOne/api/src/main/java/com.debug.mooc.dubbo.one.api/response/BaseResponse.java
@@ -0,0 +1,87 @@
+package com.debug.mooc.dubbo.one.api.response;/**
+ * Created by coderpwh on 2019/1/12.
+ */
+
+import com.debug.mooc.dubbo.one.api.enums.StatusCode;
+
+import java.io.Serializable;
+
+/**
+ * @Author:coderpwh
+ * @Date: 2019/5/30 14:14
+ **/
+public class BaseResponse<T> implements Serializable{
+
+    private Integer code;
+    private String msg;
+    private T data;
+
+    public BaseResponse() {
+    }
+
+    public BaseResponse(Integer code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public BaseResponse(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public BaseResponse(StatusCode statusCode) {
+        this.code = statusCode.getCode();
+        this.msg = statusCode.getMsg();
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
