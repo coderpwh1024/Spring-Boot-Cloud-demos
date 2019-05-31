d5f04bbc9c7a68318d3688e87c693ff1f50dfd25
coderpwh <pengwenhao@aliyun.com>
2019-05-31 10:50:58 +0800

    Add files via upload
    add entity package

---
+++ dubbo/dubboOne/model/src/main/java/com/debug/mooc/dubbo/one/model/entity/ItemInfo.java
@@ -0,0 +1,83 @@
+package com.debug.mooc.dubbo.one.model.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * 2 * @Author: coderpwh
+ * 3 * @Date: 2019/5/31 10:24
+ * 4
+ */
+public class ItemInfo implements Serializable {
+
+    private Integer id;
+
+    private String code;
+
+    private String name;
+
+    private BigDecimal price;
+
+    private Integer isActive;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Integer isActive) {
+        this.isActive = isActive;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
