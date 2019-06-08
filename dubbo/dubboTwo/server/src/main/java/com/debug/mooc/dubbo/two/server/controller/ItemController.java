c14c586807bfdf8fdc7029186cb501f91c55d373
coder-PengWenHao <pengwenhao@aliyun.com>
2019-06-08 23:16:40 +0800

    update dubbo demo

--- dubbo/dubboTwo/server/src/main/java/com/debug/mooc/dubbo/two/server/controller/ItemController.java
+++ dubbo/dubboTwo/server/src/main/java/com/debug/mooc/dubbo/two/server/controller/ItemController.java
@@ -30,6 +30,32 @@ public class ItemController {
     private IDubboItemService dubboItemService;
 
 
+    /**
+     * 用户商城列表查询
+     * @return
+     */
+    @RequestMapping(value = prefix+"/list",method = RequestMethod.GET)
+    public Map<String,Object> list(){
+        Map<String,Object> resMap= Maps.newHashMap();
+        resMap.put("code","0");
+        resMap.put("msg","成功");
+
+        //TODO:调用服务提供方dubboOne提供的列表查询功能
+        try {
+            BaseResponse response=dubboItemService.listItems();
+            if (response!=null && response.getCode().equals(0)){
+                resMap.put("data",response.getData());
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            resMap.put("code","-1");
+            resMap.put("msg","失败");
+        }
+        return resMap;
+    }
+
+
     @RequestMapping(value = prefix+"/page/list",method = RequestMethod.GET)
     public Map<String,Object> pageList(Integer pageNo,Integer pageSize){
         if (pageNo==null || pageSize==null || pageNo<=0 || pageSize<=0){
@@ -44,6 +70,39 @@ public class ItemController {
         //TODO:调用服务提供方dubboOne提供的列表查询-分页查询功能
         try {
             BaseResponse response=dubboItemService.listPageItems(pageNo,pageSize);
+            if (response!=null && response.getCode().equals(0)){
+                resMap.put("data",response.getData());
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            resMap.put("code","-1");
+            resMap.put("msg","失败");
+        }
+        return resMap;
+    }
+
+
+
+
+
+    /**
+     * 用户商城列表查询-分页查询-带参数模糊查询
+     * @return
+     */
+    @RequestMapping(value = prefix+"/page/list/params",method = RequestMethod.GET)
+    public Map<String,Object> pageListParams(Integer pageNo,Integer pageSize,String search){
+        if (pageNo==null || pageSize==null || pageNo<=0 || pageSize<=0){
+            pageNo=1;
+            pageSize=2;
+        }
+        Map<String,Object> resMap= Maps.newHashMap();
+        resMap.put("code","0");
+        resMap.put("msg","成功");
+
+        //TODO:调用服务提供方dubboOne提供的列表查询-分页查询功能
+        try {
+            BaseResponse response=dubboItemService.listPageItemsParams(pageNo,pageSize,search);
             if (response!=null && response.getCode().equals(0)){
                 resMap.put("data",response.getData());
 
