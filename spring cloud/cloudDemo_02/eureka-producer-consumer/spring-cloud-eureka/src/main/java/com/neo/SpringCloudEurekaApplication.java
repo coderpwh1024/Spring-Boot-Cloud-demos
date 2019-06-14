de8075d63edd481986616df36e0f096051553c84
coder-PengWenHao <pengwenhao@aliyun.com>
2019-06-14 23:09:57 +0800

    update cloud demo

---
+++ spring cloud/cloudDemo_02/eureka-producer-consumer/spring-cloud-eureka/src/main/java/com/neo/SpringCloudEurekaApplication.java
@@ -0,0 +1,18 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author coderpwh
+ * @create 2019-06-14 22:52
+ * @desc ${DESCRIPTION}
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaApplication.class, args);
+    }
+}
