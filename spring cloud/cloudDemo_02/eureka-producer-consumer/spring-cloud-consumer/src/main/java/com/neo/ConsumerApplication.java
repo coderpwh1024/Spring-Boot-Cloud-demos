1139b38ec7ae5c20f3c4a18f25734eb62a2106b9
coder-PengWenHao <pengwenhao@aliyun.com>
2019-06-16 14:40:10 +0800

    updte demos

---
+++ spring cloud/cloudDemo_02/eureka-producer-consumer/spring-cloud-consumer/src/main/java/com/neo/ConsumerApplication.java
@@ -0,0 +1,22 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+
+/**
+ * @author coderpwh
+ * @create 2019-06-16 14:35
+ * @desc ${DESCRIPTION}
+ **/
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
