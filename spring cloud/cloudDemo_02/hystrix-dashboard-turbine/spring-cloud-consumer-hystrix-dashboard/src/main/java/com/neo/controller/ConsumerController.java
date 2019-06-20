83dabac8ae08456f9eb5a8e8716f6193eb2e80b6
coderpwh <pengwenhao@aliyun.com>
2019-06-20 18:49:23 +0800

    Add files via upload
    add spring-cloud-consumer-hystrix-dashboard

---
+++ spring cloud/cloudDemo_02/hystrix-dashboard-turbine/spring-cloud-consumer-hystrix-dashboard/src/main/java/com/neo/controller/ConsumerController.java
@@ -0,0 +1,19 @@
+package com.neo.controller;
+
+import com.neo.remote.HelloRemote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    HelloRemote helloRemote;
+
+    @RequestMapping("/hello/{name}")
+    public String index(@PathVariable("name") String name) {
+        return helloRemote.hello(name);
+    }
+}
---
+++ spring cloud/cloudDemo_02/hystrix-dashboard-turbine/spring-cloud-consumer-hystrix-dashboard/src/main/java/com/neo/remote/HelloRemoteHystrix.java
@@ -0,0 +1,15 @@
+package com.neo.remote;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Component
+public class HelloRemoteHystrix  implements HelloRemote{
+
+    @Override
+    public String hello(@RequestParam(value = "name") String name) {
+        return "hello " +name+", this messge send failed ";
+    }
+
+
+}
