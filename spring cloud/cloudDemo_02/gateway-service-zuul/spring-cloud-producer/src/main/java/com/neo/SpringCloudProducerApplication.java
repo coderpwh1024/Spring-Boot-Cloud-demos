d29c15fc00599c65444a8c2ab5759daba07a4340
coder-PengWenHao <pengwenhao@aliyun.com>
2019-07-04 23:18:49 +0800

    update gateway-service-zuul

---
+++ spring cloud/cloudDemo_02/gateway-service-zuul/spring-cloud-producer/src/main/java/com/neo/SpringCloudProducerApplication.java
@@ -0,0 +1,13 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudProducerApplication.class, args);
+	}
+
+}
