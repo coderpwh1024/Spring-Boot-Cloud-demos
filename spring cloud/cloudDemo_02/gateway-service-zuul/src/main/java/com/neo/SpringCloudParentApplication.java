b6d32756b8813e06cf8b0992efe4482856c84284
coder-PengWenHao <pengwenhao@aliyun.com>
2019-07-04 22:58:16 +0800

    update gateway-service-zuul

--- spring cloud/cloudDemo_02/gateway-service-zuul/src/main/java/com/neo/SpringCloudParentApplication.java
+++ spring cloud/cloudDemo_02/gateway-service-zuul/src/main/java/com/neo/SpringCloudParentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringCloudParentApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringCloudParentApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudParentApplication.class, args);
+	}
 
 }
