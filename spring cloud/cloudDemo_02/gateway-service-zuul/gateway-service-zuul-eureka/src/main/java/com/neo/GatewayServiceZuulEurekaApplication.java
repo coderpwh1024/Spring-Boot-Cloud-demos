b9f665c3427054ed970011e3c3318891585ec04f
coder-PengWenHao <pengwenhao@aliyun.com>
2019-07-06 17:32:46 +0800

    update gateway-service-zuul

--- spring cloud/cloudDemo_02/gateway-service-zuul/gateway-service-zuul-eureka/src/main/java/com/neo/GatewayServiceZuulEurekaApplication.java
+++ spring cloud/cloudDemo_02/gateway-service-zuul/gateway-service-zuul-eureka/src/main/java/com/neo/GatewayServiceZuulEurekaApplication.java
@@ -2,8 +2,10 @@ package com.neo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class GatewayServiceZuulEurekaApplication {
 
 	public static void main(String[] args) {
