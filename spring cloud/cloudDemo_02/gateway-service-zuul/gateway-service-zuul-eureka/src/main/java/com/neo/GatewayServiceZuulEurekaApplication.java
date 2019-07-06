0918b57481687aaaddffbabc437267ca27aaf7dc
coder-PengWenHao <pengwenhao@aliyun.com>
2019-07-06 17:25:16 +0800

    update gateway-service-zuul

---
+++ spring cloud/cloudDemo_02/gateway-service-zuul/gateway-service-zuul-eureka/src/main/java/com/neo/GatewayServiceZuulEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceZuulEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceZuulEurekaApplication.class, args);
+	}
+
+}
