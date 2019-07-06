dc582a111f73ad5d69ae0f08f4f72d539445f00b
coder-PengWenHao <pengwenhao@aliyun.com>
2019-07-06 16:53:01 +0800

    update gateway-service-zuul

---
+++ spring cloud/cloudDemo_02/gateway-service-zuul/gateway-service-zuul-simple/src/main/java/com/neo/GatewayServiceZuulSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceZuulSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceZuulSimpleApplication.class, args);
+	}
+
+}
