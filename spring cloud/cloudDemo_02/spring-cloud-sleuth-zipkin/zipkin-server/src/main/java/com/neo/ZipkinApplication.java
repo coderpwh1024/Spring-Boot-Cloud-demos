003884eb6b7489570b0c6802e142bacce74b8134
coderpwh <pengwenhao@aliyun.com>
2019-07-25 20:16:39 +0800

    Add files via upload
    add zipkin-server

---
+++ spring cloud/cloudDemo_02/spring-cloud-sleuth-zipkin/zipkin-server/src/main/java/com/neo/ZipkinApplication.java
@@ -0,0 +1,18 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+
+}
