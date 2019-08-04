897ab749fd75b94e50e9e9b707ef9f40224e4e05
coder-PengWenHao <pengwenhao@aliyun.com>
2019-08-04 14:08:58 +0800

    update sharding-jdbc

---
+++ sharding-jdbc demo/sharding-jdbc-demo01/src/main/java/com/coderpwh/ShardingJdbcDemo01Application.java
@@ -0,0 +1,13 @@
+package com.coderpwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingJdbcApplication.class, args);
+	}
+
+}
