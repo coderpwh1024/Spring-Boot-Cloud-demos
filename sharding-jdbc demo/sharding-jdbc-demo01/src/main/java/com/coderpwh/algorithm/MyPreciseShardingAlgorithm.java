d340cc4b13323f5fabce94043ccb1c82cdbf5a62
coder-PengWenHao <pengwenhao@aliyun.com>
2019-08-04 14:51:27 +0800

    update sharding-jdbc

---
+++ sharding-jdbc demo/sharding-jdbc-demo01/src/main/java/com/coderpwh/algorithm/MyPreciseShardingAlgorithm.java
@@ -0,0 +1,26 @@
+package com.coderpwh.algorithm;
+
+import org.apache.shardingsphere.api.sharding.standard.PreciseShardingAlgorithm;
+import org.apache.shardingsphere.api.sharding.standard.PreciseShardingValue;
+
+import java.util.Collection;
+
+/**
+ * @author coderpwh
+ * @create 2019-08-04 14:48
+ * @desc ${DESCRIPTION}
+ **/
+public class MyPreciseShardingAlgorithm implements PreciseShardingAlgorithm<Long> {
+
+
+
+    @Override
+    public String doSharding(Collection<String> availableTargetNames, PreciseShardingValue<Long> shardingValue) {
+        for (String tableName : availableTargetNames) {
+            if (tableName.endsWith(shardingValue.getValue() % 4 + "")) {
+                return tableName;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+}
