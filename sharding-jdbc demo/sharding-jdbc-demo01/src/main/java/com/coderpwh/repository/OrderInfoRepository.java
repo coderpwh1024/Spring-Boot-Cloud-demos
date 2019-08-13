ef057db106ffb1c0dff65bed69f794a62bd626a9
coder-PengWenHao <pengwenhao@aliyun.com>
2019-08-13 15:09:28 +0800

    update sharding-jdbc

---
+++ sharding-jdbc demo/sharding-jdbc-demo01/src/main/java/com/coderpwh/repository/OrderInfoRepository.java
@@ -0,0 +1,14 @@
+package com.coderpwh.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author coderpwh
+ * @create 2019-08-13 15:07
+ * @desc ${DESCRIPTION}
+ **/
+@Mapper
+public interface OrderInfoRepository {
+
+
+}
