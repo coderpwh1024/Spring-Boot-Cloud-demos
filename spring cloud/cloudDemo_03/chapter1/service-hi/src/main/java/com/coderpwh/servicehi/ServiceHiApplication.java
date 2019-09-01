aabaceaf08d25201dc816bc85a936589a49c24f2
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-01 16:29:01 +0800

    update spring cloud demo

---
+++ spring cloud/cloudDemo_03/chapter1/service-hi/src/main/java/com/coderpwh/servicehi/ServiceHiApplication.java
@@ -0,0 +1,13 @@
+package com.coderpwh.servicehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHiApplication.class, args);
+	}
+
+}
