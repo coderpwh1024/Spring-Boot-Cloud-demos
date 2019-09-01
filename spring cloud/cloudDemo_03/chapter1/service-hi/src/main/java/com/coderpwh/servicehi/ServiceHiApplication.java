969d347c590cd14f72e01afceb0c9334803f1b69
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-01 16:45:09 +0800

    update spring cloud

--- spring cloud/cloudDemo_03/chapter1/service-hi/src/main/java/com/coderpwh/servicehi/ServiceHiApplication.java
+++ spring cloud/cloudDemo_03/chapter1/service-hi/src/main/java/com/coderpwh/servicehi/ServiceHiApplication.java
@@ -2,12 +2,14 @@ package com.coderpwh.servicehi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceHiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceHiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHiApplication.class, args);
+    }
 
 }
