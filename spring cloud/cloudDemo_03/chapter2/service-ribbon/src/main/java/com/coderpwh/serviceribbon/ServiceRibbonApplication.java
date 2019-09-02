60e48542d2657e4a10c87429f4e57be2c8272ec4
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-02 23:08:58 +0800

    update spring cloud

---
+++ spring cloud/cloudDemo_03/chapter2/service-ribbon/src/main/java/com/coderpwh/serviceribbon/ServiceRibbonApplication.java
@@ -0,0 +1,13 @@
+package com.coderpwh.serviceribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRibbonApplication.class, args);
+	}
+
+}
