1f3b22c267999a7dfd1a4a7de7f0b7b29c955bee
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-02 23:01:13 +0800

    update spring cloud

---
+++ spring cloud/cloudDemo_03/chapter2/service-hi/src/main/java/com/codepwh/servicehi/ServiceHiApplication.java
@@ -0,0 +1,37 @@
+package com.codepwh.servicehi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author coderpwh
+ * @create 2019-09-02 22:58
+ * @desc ${DESCRIPTION}
+ **/
+
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ServiceHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ServiceHiApplication.class, args );
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+
+}
