3a6f7b46888e2c7133e691338db3b09766c740b5
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-02 23:15:33 +0800

    update spring cloud

---
+++ spring cloud/cloudDemo_03/chapter2/service-ribbon/src/main/java/com/coderpwh/serviceribbon/service/HelloService.java
@@ -0,0 +1,22 @@
+package com.coderpwh.serviceribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author coderpwh
+ * @create 2019-09-02 23:13
+ * @desc ${DESCRIPTION}
+ **/
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+}
---
+++ spring cloud/cloudDemo_03/chapter2/service-ribbon/src/main/java/com/coderpwh/serviceribbon/web/HelloControler.java
@@ -0,0 +1,25 @@
+package com.coderpwh.serviceribbon.web;
+
+import com.coderpwh.serviceribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author coderpwh
+ * @create 2019-09-02 23:14
+ * @desc ${DESCRIPTION}
+ **/
+
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService( name );
+    }
+}
