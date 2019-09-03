77b9581c060bfebacbe75dacec4326dc36cf0b1a
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-03 22:58:04 +0800

    update sprig cloud

---
+++ spring cloud/cloudDemo_03/chapter3/service-feign/src/main/java/com/coderpwh/servicefeign/clients/SchedualServiceHi.java
@@ -0,0 +1,19 @@
+package com.coderpwh.servicefeign.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author coderpwh
+ * @create 2019-09-03 22:55
+ * @desc ${DESCRIPTION}
+ **/
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+
+
+}
---
+++ spring cloud/cloudDemo_03/chapter3/service-feign/src/main/java/com/coderpwh/servicefeign/web/HiController.java
@@ -0,0 +1,25 @@
+package com.coderpwh.servicefeign.web;
+
+import com.coderpwh.servicefeign.clients.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author coderpwh
+ * @create 2019-09-03 22:56
+ * @desc ${DESCRIPTION}
+ **/
+@RestController
+public class HiController {
+
+    //编译器报错，无视。 因为这个Bean是在程序启动的时候注入的，编译器感知不到，所以报错。
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @GetMapping(value = "/hi")
+    public String sayHi(@RequestParam String name) {
+        return schedualServiceHi.sayHiFromClientOne( name );
+    }
+}
