6e7de80e5c90118084aa6bac9de5e3ac1639f107
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-07 16:25:55 +0800

    update spring cloud

---
+++ spring cloud/cloudDemo_03/chapter5/service-feign/src/main/java/com/coderpwh/servicefeign/clients/fallback/SchedualServiceHiHystric.java
@@ -0,0 +1,18 @@
+package com.coderpwh.servicefeign.clients.fallback;
+
+import com.coderpwh.servicefeign.clients.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+/**
+ * Email miles02@163.com
+ *
+ * @author fangzhipeng
+ * create 2018-07-09
+ **/
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry, you are fail,"+name;
+    }
+}
