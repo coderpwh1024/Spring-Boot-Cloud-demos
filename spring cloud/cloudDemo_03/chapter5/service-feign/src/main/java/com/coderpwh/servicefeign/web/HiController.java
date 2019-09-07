e07f84832d48df1d5f8367cc1eabaf7ff7f15a84
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-07 16:53:10 +0800

    update srping cloud chapter5

--- spring cloud/cloudDemo_03/chapter5/service-feign/src/main/java/com/coderpwh/servicefeign/web/HiController.java
+++ spring cloud/cloudDemo_03/chapter5/service-feign/src/main/java/com/coderpwh/servicefeign/web/HiController.java
@@ -1,6 +1,6 @@
 package com.coderpwh.servicefeign.web;
 
-import com.forezp.servicefeign.clients.SchedualServiceHi;
+import com.coderpwh.servicefeign.clients.SchedualServiceHi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
---
+++ spring cloud/cloudDemo_03/chapter5/service-zuul/src/main/java/com/coderpwh/servicezuul/filter/MyFilter.java
@@ -0,0 +1,64 @@
+package com.coderpwh.servicezuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author coderpwh
+ * @create 2019-09-07 16:43
+ * @desc ${DESCRIPTION}
+ **/
+
+
+@Component
+public class MyFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if (accessToken == null) {
+
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (Exception e) {
+            }
+
+            return null;
+
+        }
+        log.info("ok");
+        return null;
+
+    }
+
+}
