71a01ffffcc04c4139e485f9ed6d40fea5e3887b
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-08 14:47:35 +0800

    update spring cloud chapter6

---
+++ spring cloud/cloudDemo_03/chapter6/src/main/java/com/coderpwh/chapter6/Chapter6Application.java
@@ -0,0 +1,13 @@
+package com.coderpwh.chapter6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter6Application.class, args);
+	}
+
+}
