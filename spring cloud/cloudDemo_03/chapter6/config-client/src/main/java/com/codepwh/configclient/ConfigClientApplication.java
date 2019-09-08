b38b6416451a51683b3c6472179c6385a5cc0d5a
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-08 15:35:48 +0800

    update spring cloud chapter6

--- spring cloud/cloudDemo_03/chapter6/config-client/src/main/java/com/codepwh/configclient/ConfigClientApplication.java
+++ spring cloud/cloudDemo_03/chapter6/config-client/src/main/java/com/codepwh/configclient/ConfigClientApplication.java
@@ -1,13 +1,26 @@
 package com.codepwh.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ConfigClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi() {
+        return foo;
+    }
 
 }
