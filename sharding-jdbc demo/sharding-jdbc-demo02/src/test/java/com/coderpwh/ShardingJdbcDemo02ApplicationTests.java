0dd1749352dae28874f2ebcc6640ff40b95a0139
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-14 23:51:31 +0800

    update sharding-jdbc

--- sharding-jdbc demo/sharding-jdbc-demo02/src/test/java/com/coderpwh/ShardingJdbcDemo02ApplicationTests.java
+++ sharding-jdbc demo/sharding-jdbc-demo02/src/test/java/com/coderpwh/ShardingJdbcDemo02ApplicationTests.java
@@ -1,16 +1,16 @@
 package com.coderpwh;
 
-import org.junit.Test;
+/*import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest*/
 public class ShardingJdbcDemo02ApplicationTests {
-
+/*
 	@Test
 	public void contextLoads() {
-	}
+	}*/
 
 }
