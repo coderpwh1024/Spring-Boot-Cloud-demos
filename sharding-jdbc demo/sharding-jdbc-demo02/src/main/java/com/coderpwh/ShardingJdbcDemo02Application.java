84dd7513b5b59dec0d6be3323f7e7a63a54af8fc
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-14 23:27:00 +0800

    update sharding-jdbc

---
+++ sharding-jdbc demo/sharding-jdbc-demo02/src/main/java/com/coderpwh/ShardingJdbcDemo02Application.java
@@ -0,0 +1,13 @@
+package com.coderpwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJdbcDemo02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingJdbcDemo02Application.class, args);
+	}
+
+}
---
+++ sharding-jdbc demo/sharding-jdbc-demo02/src/main/java/com/coderpwh/util/Test.java
@@ -0,0 +1,20 @@
+package com.coderpwh.util;
+
+/**
+ * @author coderpwh
+ * @create 2019-08-03 23:27
+ * @desc ${DESCRIPTION}
+ **/
+public class Test {
+
+
+    public static void main(String[] args) {
+
+
+        Integer a =12;
+        System.out.println(a.longValue());
+        long b =15544L;
+        System.out.println(b);
+
+    }
+}
