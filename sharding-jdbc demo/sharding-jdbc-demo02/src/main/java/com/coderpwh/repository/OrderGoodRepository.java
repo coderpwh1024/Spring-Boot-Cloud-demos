f9569cf9e6f79a73dc1abc6dabc6e293630844b2
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-18 23:09:23 +0800

    update orderGood

---
+++ sharding-jdbc demo/sharding-jdbc-demo02/src/main/java/com/coderpwh/repository/OrderGoodRepository.java
@@ -0,0 +1,13 @@
+package com.coderpwh.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author coderpwh
+ * @create 2019-09-18 23:08
+ * @desc ${DESCRIPTION}
+ **/
+@Mapper
+public interface OrderGoodRepository {
+
+}
