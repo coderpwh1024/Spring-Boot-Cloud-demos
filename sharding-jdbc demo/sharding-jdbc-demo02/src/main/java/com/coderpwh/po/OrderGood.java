18a29790edd57fd577841d8897b261562fc129ff
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-18 23:07:28 +0800

    add orderGood

---
+++ sharding-jdbc demo/sharding-jdbc-demo02/src/main/java/com/coderpwh/po/OrderGood.java
@@ -0,0 +1,82 @@
+package com.coderpwh.po;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class OrderGood implements Serializable {
+
+    private static final long serialVersionUID = -5661798916692942285L;
+
+    private Integer ogId;
+
+
+    private Long orderId;
+
+
+    private String orderNo;
+
+
+    private String packageNo;
+
+
+    private Integer goodId;
+
+
+    private Integer saleId;
+
+
+    private String goodSku;
+
+
+    private String goodName;
+
+
+    private Integer goodNum;
+
+
+    private Integer goodPrice;
+
+
+    private Integer goodAmount;
+
+
+    private String modelName;
+
+
+    private String virtualFlag;
+
+
+    private Integer virtualType;
+
+
+    private String goodImage;
+
+
+    private Integer discountAmount;
+
+
+    private Integer couponAmount;
+
+
+    private String snapshotData;
+
+
+    private Integer goldPrice;
+
+
+    private Integer memberPrice;
+
+
+    private Integer logisticsAmount;
+
+
+    private Integer delFlag;
+
+
+    private Long createrId;
+
+    private Date createrTime;
+
+
+}
