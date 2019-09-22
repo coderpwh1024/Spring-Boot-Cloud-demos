bfd5008567ba13c11511a3403433e984080c3769
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-22 16:08:21 +0800

    update sharding-jdbc

---
+++ sharding-jdbc demo/sharding-jdbc-demo02/src/main/java/com/coderpwh/po/OrderPayment.java
@@ -0,0 +1,144 @@
+package com.coderpwh.po;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author coderpwh
+ * @create 2019-09-22 16:05
+ * @desc ${DESCRIPTION}
+ **/
+public class OrderPayment implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;
+
+
+    private Integer payId;
+
+
+    private Long orderId;
+
+
+    private String orderNo;
+
+
+    private String payType;
+
+
+    private Integer payAmount;
+
+
+    private Date payTime;
+
+
+    private Integer payStatus;
+
+
+    private String payNo;
+
+
+    private String payDesc;
+
+
+    private Long createrId;
+
+
+    private Date createrTime;
+
+    public OrderPayment() {
+
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getPayId() {
+        return payId;
+    }
+
+    public void setPayId(Integer payId) {
+        this.payId = payId;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public String getPayType() {
+        return payType;
+    }
+
+    public void setPayType(String payType) {
+        this.payType = payType;
+    }
+
+    public Integer getPayAmount() {
+        return payAmount;
+    }
+
+    public void setPayAmount(Integer payAmount) {
+        this.payAmount = payAmount;
+    }
+
+    public Date getPayTime() {
+        return payTime;
+    }
+
+    public void setPayTime(Date payTime) {
+        this.payTime = payTime;
+    }
+
+    public Integer getPayStatus() {
+        return payStatus;
+    }
+
+    public void setPayStatus(Integer payStatus) {
+        this.payStatus = payStatus;
+    }
+
+    public String getPayNo() {
+        return payNo;
+    }
+
+    public void setPayNo(String payNo) {
+        this.payNo = payNo;
+    }
+
+    public String getPayDesc() {
+        return payDesc;
+    }
+
+    public void setPayDesc(String payDesc) {
+        this.payDesc = payDesc;
+    }
+
+    public Long getCreaterId() {
+        return createrId;
+    }
+
+    public void setCreaterId(Long createrId) {
+        this.createrId = createrId;
+    }
+
+    public Date getCreaterTime() {
+        return createrTime;
+    }
+
+    public void setCreaterTime(Date createrTime) {
+        this.createrTime = createrTime;
+    }
+}
