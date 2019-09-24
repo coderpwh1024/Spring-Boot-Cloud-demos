c60782bd354e7fe6b98b13b960d99304e6017a0c
coder-PengWenHao <pengwenhao@aliyun.com>
2019-09-24 23:37:16 +0800

    update sharding-jdbc

--- sharding-jdbc demo/sharding-jdbc-demo02/src/main/java/com/coderpwh/vo/OrderInfoListVo.java
+++ sharding-jdbc demo/sharding-jdbc-demo02/src/main/java/com/coderpwh/vo/OrderInfoListVo.java
@@ -1,9 +1,27 @@
 package com.coderpwh.vo;
 
+import java.util.Date;
+
 /**
  * @author coderpwh
  * @create 2019-09-24 23:24
  * @desc ${DESCRIPTION}
  **/
 public class OrderInfoListVo {
+
+    private String orderNo;
+
+    private Date createDate;
+
+    private String goodName;
+
+
+    private String goodId;
+
+    private String goodSku;
+
+
+    private String goodImage;
+
+
 }
