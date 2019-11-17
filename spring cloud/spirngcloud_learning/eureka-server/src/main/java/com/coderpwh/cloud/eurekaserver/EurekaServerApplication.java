55c91ca1d490294e26667534286c3d990ee5f907
coder-PengWenHao <pengwenhao@aliyun.com>
2019-11-17 15:50:57 +0800

    update spring cloud

---
+++ spring cloud/spirngcloud_learning/eureka-server/src/main/java/com/coderpwh/cloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.coderpwh.cloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
