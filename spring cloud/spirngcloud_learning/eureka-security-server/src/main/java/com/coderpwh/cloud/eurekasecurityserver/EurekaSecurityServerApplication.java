3d865ccc1544cd35b2f87cee9749dd43e83899e9
coder-PengWenHao <pengwenhao@aliyun.com>
2019-11-18 22:53:54 +0800

    update spring cloud

---
+++ spring cloud/spirngcloud_learning/eureka-security-server/src/main/java/com/coderpwh/cloud/eurekasecurityserver/EurekaSecurityServerApplication.java
@@ -0,0 +1,13 @@
+package com.coderpwh.cloud.eurekasecurityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSecurityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSecurityServerApplication.class, args);
+	}
+
+}
