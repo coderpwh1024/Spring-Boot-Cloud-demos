30c4ae05911922788de7077cbc24b954a15a6fa0
coder-PengWenHao <pengwenhao@aliyun.com>
2019-11-18 23:13:56 +0800

    update spring cloud

--- spring cloud/spirngcloud_learning/eureka-security-server/src/main/java/com/coderpwh/cloud/eurekasecurityserver/config/WebSecurityConfig.java
+++ spring cloud/spirngcloud_learning/eureka-security-server/src/main/java/com/coderpwh/cloud/eurekasecurityserver/config/WebSecurityConfig.java
@@ -1,10 +1,20 @@
 package com.coderpwh.cloud.eurekasecurityserver.config;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 /**
  * @author coderpwh
  * @create 2019-11-18 22:58
  * @desc ${DESCRIPTION}
  **/
-public class WebSecurityConfig {
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
 }
