53b56f116a753c6986ae30becb7ac4ab5af26a60
coder-PengWenHao <pengwenhao@aliyun.com>
2019-11-18 23:02:05 +0800

    update spring cloud

---
+++ spring cloud/spirngcloud_learning/eureka-security-server/src/main/java/com/coderpwh/cloud/eurekasecurityserver/config/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.coderpwh.cloud.eurekasecurityserver.config;
+
+/**
+ * @author coderpwh
+ * @create 2019-11-18 22:58
+ * @desc ${DESCRIPTION}
+ **/
+public class WebSecurityConfig {
+
+}
