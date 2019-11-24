a780263b8da797f9a100768b6c6c2c92fd945841
coder-PengWenHao <pengwenhao@aliyun.com>
2019-11-24 17:03:18 +0800

    update spring cloud

--- spring cloud/spirngcloud_learning/ribbon-service/src/main/java/com/coderpwh/cloud/ribbonservice/RibbonServiceApplication.java
+++ spring cloud/spirngcloud_learning/ribbon-service/src/main/java/com/coderpwh/cloud/ribbonservice/RibbonServiceApplication.java
@@ -2,8 +2,10 @@ package com.coderpwh.cloud.ribbonservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RibbonServiceApplication {
 
 	public static void main(String[] args) {
