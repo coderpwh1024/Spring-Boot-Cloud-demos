459eb152d6e6f8c0eb20b4f636542d032d3a1d30
coder-PengWenHao <pengwenhao@aliyun.com>
2020-02-28 15:01:34 +0800

    add spring cloud demo

---
+++ spring cloud/cloudDemo_03/gateway-filter/src/main/java/com/coderpwh/gatewayfilter/GatewayFilterApplication.java
@@ -0,0 +1,13 @@
+package com.coderpwh.gatewayfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayFilterApplication.class, args);
+	}
+
+}
---
+++ spring cloud/cloudDemo_03/gateway-filter/src/test/java/com/coderpwh/gatewayfilter/GatewayFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderpwh.gatewayfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
