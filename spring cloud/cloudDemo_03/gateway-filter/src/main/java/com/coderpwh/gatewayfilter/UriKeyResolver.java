0c0c20869fc7c4d305c2c4932fe3a74a33fbbd15
coder-PengWenHao <pengwenhao@aliyun.com>
2020-02-28 15:22:35 +0800

    update spring cloud code

---
+++ spring cloud/cloudDemo_03/gateway-filter/src/main/java/com/coderpwh/gatewayfilter/UriKeyResolver.java
@@ -0,0 +1,18 @@
+package com.coderpwh.gatewayfilter;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author coderpwh
+ * @create 2020-02-28 15:20
+ * @desc ${DESCRIPTION}
+ **/
+public class UriKeyResolver implements KeyResolver {
+
+    @Override
+    public Mono<String> resolve(ServerWebExchange exchange) {
+        return Mono.just(exchange.getRequest().getURI().getPath());
+    }
+}
