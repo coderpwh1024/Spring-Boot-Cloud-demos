4e05ee9331e83182c66f06462214752e8e7fc8bd
coder-PengWenHao <pengwenhao@aliyun.com>
2020-03-08 14:49:47 +0800

    update spring cloud

---
+++ spring boots/springboot-limit/src/main/java/com/coderpwh/limit/LimitApplication.java
@@ -0,0 +1,13 @@
+package com.coderpwh.limit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitApplication.class, args);
+	}
+
+}
