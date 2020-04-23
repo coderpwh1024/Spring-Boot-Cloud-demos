78c3da52af194dce0709399875682fe0993bebe4
pengwenhao <pengwenhao@woaizuji.com>
2020-04-23 17:16:37 +0800

    update Rabbitmq Demo code

--- spring boots/RabbimqDemo/rabbitmq-demo/src/main/java/com/coderpwh/rabbitmq/config/RabbitConfig.java
+++ spring boots/RabbimqDemo/rabbitmq-demo/src/main/java/com/coderpwh/rabbitmq/config/RabbitConfig.java
@@ -1,9 +1,10 @@
 package com.coderpwh.rabbitmq.config;
 
 
+import com.coderpwh.rabbitmq.message.Demo01Message;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Queue;
+import org.springframework.amqp.core.*;
 
 /**
  * @description:
@@ -14,16 +15,41 @@ import java.util.Queue;
 public class RabbitConfig {
 
 
+    /***
+     *  Direct Exchange
+     */
+    public static class DirectExchangeDemoConfiguration {
 
-    public  static class  DirectExchangeDemoConfiguration{
+        @Bean
+        public Queue demo01Queue() {
+            return new Queue(Demo01Message.QUEUE, // Queue 名字
+                    true, // durable: 是否持久化
+                    false, // exclusive: 是否排它
+                    false); // autoDelete: 是否自动删除
+        }
 
-         /*public Queue demo01Queue(){
-             return new Queue(Demo01Message.QUEUE, // Queue 名字
-                     true, // durable: 是否持久化
-                     false, // exclusive: 是否排它
-                     false); // autoDelete: 是否自动删除
-         }*/
 
+        /***
+         *  创建 Direct Exchange
+         * @return
+         */
+        @Bean
+        public DirectExchange demo01Exchange() {
+
+            return new DirectExchange(Demo01Message.EXCHANGE,
+                    true,  // durable: 是否持久化
+                    false);  // exclusive: 是否排它
+        }
+
+        /***创建 Binding
+         Exchange：Demo01Message.EXCHANGE
+         Routing key：Demo01Message.ROUTING_KEY
+         Queue：Demo01Message.QUEUE
+         */
+        @Bean
+        public Binding demo01Binding() {
+            return BindingBuilder.bind(demo01Queue()).to(demo01Exchange()).with(Demo01Message.ROUTING_KEY);
+        }
 
 
     }
