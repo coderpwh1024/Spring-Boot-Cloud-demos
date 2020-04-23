57454c95a0a82b570b90c51d10f38ccae1be3059
pengwenhao <pengwenhao@woaizuji.com>
2020-04-23 17:39:05 +0800

    update RabbitMQ code

--- spring boots/RabbimqDemo/rabbitmq-demo/src/main/java/com/coderpwh/rabbitmq/config/RabbitConfig.java
+++ spring boots/RabbimqDemo/rabbitmq-demo/src/main/java/com/coderpwh/rabbitmq/config/RabbitConfig.java
@@ -3,6 +3,7 @@ package com.coderpwh.rabbitmq.config;
 
 import com.coderpwh.rabbitmq.message.Demo01Message;
 import com.coderpwh.rabbitmq.message.Demo02Message;
+import com.coderpwh.rabbitmq.message.Demo03Message;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.*;
@@ -58,7 +59,7 @@ public class RabbitConfig {
 
     /****
      *   Topic 模式
-     *   
+     *
      */
 
     public static class TopicExchangeDemoConfiguration {
@@ -91,6 +92,56 @@ public class RabbitConfig {
 
 
     }
+
+    /**
+     * Fanout Exchange 示例的配置类
+     */
+    public static class FanoutExchangeDemoConfiguration {
+
+        // 创建 Queue A
+        @Bean
+        public Queue demo03QueueA() {
+            return new Queue(Demo03Message.QUEUE_A, // Queue 名字
+                    true, // durable: 是否持久化
+                    false, // exclusive: 是否排它
+                    false); // autoDelete: 是否自动删除
+        }
+
+        // 创建 Queue B
+        @Bean
+        public Queue demo03QueueB() {
+            return new Queue(Demo03Message.QUEUE_B, // Queue 名字
+                    true, // durable: 是否持久化
+                    false, // exclusive: 是否排它
+                    false); // autoDelete: 是否自动删除
+        }
+
+        // 创建 Fanout Exchange
+        @Bean
+        public FanoutExchange demo03Exchange() {
+            return new FanoutExchange(Demo03Message.EXCHANGE,
+                    true,  // durable: 是否持久化
+                    false);  // exclusive: 是否排它
+        }
+
+        // 创建 Binding A
+        // Exchange：Demo03Message.EXCHANGE
+        // Queue：Demo03Message.QUEUE_A
+        @Bean
+        public Binding demo03BindingA() {
+            return BindingBuilder.bind(demo03QueueA()).to(demo03Exchange());
+        }
+
+        // 创建 Binding B
+        // Exchange：Demo03Message.EXCHANGE
+        // Queue：Demo03Message.QUEUE_B
+        @Bean
+        public Binding demo03BindingB() {
+            return BindingBuilder.bind(demo03QueueB()).to(demo03Exchange());
+        }
+
+    }
+
 
 
 }
