e5734c4f53aebaaba097bf47f8ecbfa0e363dc3e
pengwenhao <pengwenhao@aliyun.com>
2020-04-24 14:37:40 +0800

    update RabbitMq Demo code

---
+++ spring boots/RabbimqDemo/rabbitmq-demo/src/test/java/com/coderpwh/rabbitmq/producer/Demo01ProducerTest.java
@@ -0,0 +1,89 @@
+package com.coderpwh.rabbitmq.producer;
+
+import com.coderpwh.rabbitmq.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @description:
+ * @author: pengwenhao
+ * @date: 2020/4/24 0024 13:56
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class Demo01ProducerTest {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private Demo01Producer producer;
+
+
+    @Test
+    public void testSyncSend() throws InterruptedException {
+        int id = (int) (System.currentTimeMillis() / 1000);
+        producer.syncSend(id);
+        logger.info("[testSyncSend][发送编号:[{}] 发送成功]", id);
+
+        new CountDownLatch(1).await();
+        ;
+    }
+
+
+    @Test
+    public void testSyncSendDefault() throws InterruptedException {
+        int id = (int) (System.currentTimeMillis() / 1000);
+        producer.syncSendDefault(id);
+        logger.info("[testSyncSendDefault][发送编号：[{}]]", id);
+
+        // 阻塞等待
+        new CountDownLatch(1).wait();
+    }
+
+    @Test
+    public void AsyncSend() {
+        int id = (int) (System.currentTimeMillis() / 1000);
+        producer.asyncSend(id).addCallback(new ListenableFutureCallback<Void>() {
+            @Override
+            public void onFailure(Throwable e) {
+                logger.info("[testASyncSend][发送编号：[{}] 发送异常]]", id, e);
+            }
+
+            @Override
+            public void onSuccess(Void aVoid) {
+                logger.info("[testASyncSend][发送编号：[{}] 发送成功，发送成功]", id);
+            }
+        });
+
+
+    }
+
+
+    @Test
+    public void nothing() throws InterruptedException {
+        // 阻塞等待，保证消费
+        new CountDownLatch(1).await();
+    }
+
+    @Test
+    public void nothing02() throws InterruptedException {
+        for (int i = 0; i < 1000; i++) {
+            int id = (int) (System.currentTimeMillis() / 1000);
+            producer.syncSend(id);
+            logger.info("[testSyncSend][发送编号：[{}] 发送成功]", id);
+            Thread.sleep(5000L);
+        }
+        new CountDownLatch(1).await();
+
+    }
+
+
+}
