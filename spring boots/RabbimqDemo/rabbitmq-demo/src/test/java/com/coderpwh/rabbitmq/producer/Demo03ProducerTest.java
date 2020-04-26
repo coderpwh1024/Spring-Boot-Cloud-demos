283bfbd428971c3444182110ef0f3d0bcb6e2879
pengwenhao <pengwenhao@aliyun.com>
2020-04-26 16:27:23 +0800

    update RabbitMQ Demo code

---
+++ spring boots/RabbimqDemo/rabbitmq-demo/src/test/java/com/coderpwh/rabbitmq/producer/Demo03ProducerTest.java
@@ -0,0 +1,42 @@
+package com.coderpwh.rabbitmq.producer;
+
+import com.coderpwh.rabbitmq.Application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.CountDownLatch;
+
+
+/**
+ * @description:
+ * @author: pengwenhao
+ * @date: 2020/4/26 0026 16:20
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class Demo03ProducerTest {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private Demo03Producer producer;
+
+
+    @Test
+    public void testSyncSend() throws InterruptedException {
+        int id = (int) (System.currentTimeMillis() / 1000);
+        producer.syncSend(id);
+        logger.info("[testSyncSend][发送编号：[{}] 发送成功]", id);
+
+        // 阻塞等待，保证消费
+        new CountDownLatch(1).await();
+    }
+
+
+}
