05d4dbb34cf3637af2d120e9f0d16d7bed305211
pengwenhao <pengwenhao@aliyun.com>
2020-04-27 09:24:49 +0800

    update RabbbitMQ Demo code

---
+++ spring boots/RabbimqDemo/rabbitmq-demo-batch/src/test/java/com/coderpwh/rabbitmq/RabbitmqDemoBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderpwh.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqDemoBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
