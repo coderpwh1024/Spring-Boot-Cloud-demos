884a6de3f527aaf19b3790b7009b99e3f4a94211
coderpwh <pwhfbc@outlook.com>
2021-03-05 18:12:13 +0800

    Update KafKaController.java

--- spring-boot-kafka/src/main/java/com/codderpwh/kafka/controller/KafKaController.java
+++ spring-boot-kafka/src/main/java/com/codderpwh/kafka/controller/KafKaController.java
@@ -1,9 +1,12 @@
 package com.codderpwh.kafka.controller;
 
 
+import com.codderpwh.kafka.bean.KafkaMessageSender;
 import com.codderpwh.kafka.bean.Producer;
+import com.codderpwh.kafka.common.Foo1;
 import com.codderpwh.kafka.util.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -12,6 +15,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/kafka")
 public class KafKaController {
+
+    private String topicName = "Java";
+    private String message = "Java Spring Boot 2.0 Kafka Alibaba:";
+
+
+    @Autowired
+    private KafkaMessageSender kafkaMessageSender;
 
 
     @Autowired
@@ -31,4 +41,19 @@ public class KafKaController {
     }
 
 
+    @RequestMapping(value = "/send", method = RequestMethod.GET)
+    public String sendMessageToKafkaTopic() {
+        System.out.println("发送100万条消息");
+
+        for (int i = 0; i < 1000000; i++) {
+            kafkaMessageSender.send(topicName, message + i);
+        }
+
+        System.out.println("成功，发送100万条消息");
+        return "success";
+
+    }
+
+
 }
+
